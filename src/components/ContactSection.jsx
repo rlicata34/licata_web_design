ac071c3f2a0e198c867619291c00447b21926e5d
rlicata34 <rlicata34@gmail.com>
2025-05-02 14:44:02 -0400

    Submit form with AJAX

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -45,10 +45,22 @@ function ContactSection() {
     setCaptchaToken(token);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsSubmitted(true);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
   };
+
+  document.querySelector("form").addEventListener("submit", handleSubmit);
 
   return (
     <section
