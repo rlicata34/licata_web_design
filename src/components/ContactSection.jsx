9df946c67256eb631c4ee88ea66d103a37236186
rlicata34 <rlicata34@gmail.com>
2025-05-03 13:24:08 -0400

    Test form in public folder

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -38,7 +38,6 @@ function ContactSection() {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
-    console.log(e);
   };
 
   return (
