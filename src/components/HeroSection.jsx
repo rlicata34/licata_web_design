c7fa0bfdde05e604c6644ede44f99a7d2db3c73d
rlicata34 <rlicata34@gmail.com>
2025-05-07 14:38:24 -0400

    Change style of hero section for laptop

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,20 +5,24 @@ function HeroSection() {
   return (
     <section className="hero">
       <div className="hero__content">
-        <h1 className="hero__title">Simple. Smart. Responsive.</h1>
-        <img
-          src={hero_image}
-          alt="View of what a personal workspace for a developer could look like"
-          className="hero__image"
-        />
-        <p className="hero__description">
-          Elevate your online presence with Licata Web Design. We build clean,
-          responsive websites that help small businesses stand out. Ready to
-          upgrade your site and grow your business?
-        </p>
-        <a href="#contact" className="hero__button">
-          Let’s Build Your Site
-        </a>
+        <div className="hero__conatiner">
+          <h1 className="hero__title">Simple. Smart. Responsive.</h1>
+          <img
+            src={hero_image}
+            alt="View of what a personal workspace for a developer could look like"
+            className="hero__image"
+          />
+        </div>
+        <div className="hero__container">
+          <p className="hero__description">
+            Elevate your online presence with Licata Web Design. We build clean,
+            responsive websites that help small businesses stand out. Ready to
+            upgrade your site and grow your business?
+          </p>
+          <a href="#contact" className="hero__button">
+            Let’s Build Your Site
+          </a>
+        </div>
       </div>
     </section>
   );
